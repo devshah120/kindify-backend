a5d0da6e4e73234eb2ccd24bb1bb4849ca9dee7c
devshah120 <devshah120902@gmail.com>
2025-09-14 11:28:41 +0530

    bug fix

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const upload = require('../config/multer');
-
+const auth = require('../middlewares/auth');
 // router.post('/register', authController.register);            // body: { email, mobile, role }
 // router.post('/verify-register', authController.verifyRegister);// body: { email, otp }
 router.post('/login', authController.login);                  // body: { emailOrMobile }
 router.post('/verify-login', authController.verifyLogin);     // body: { email, otp }
 router.post('/trust/register', upload.single('darpanCertificate'), authController.registerTrust);
-router.get('/trust/all', authController.getAllTrusts);
+router.get('/trust/all',auth, authController.getAllTrusts);
 module.exports = router;
